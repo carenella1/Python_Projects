efcddfee9be200db732373c0b0312fdff71f6f56
Chase Arenella <carenella1@gmail.com>
2017-04-17 16:05:37 -0700

    More files

---
+++ EX22.py
@@ -0,0 +1,8 @@
+"""
+print = printing strings
+# = comment
++,-,*,/ = math symbols - Add, Subtract, Multiply, divide
+% = Modulus
+Variables
+%d,%r,%s = formatters
+intinput = input converted to an int
